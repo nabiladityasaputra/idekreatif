445c5254649fc607df14bfde34ba1ef05798f373
nabiladityasaputra <nabiladityasaputra47@gmail.com>
2025-01-14 15:13:28 +0700

    tugas

--- auth/register_process.php
+++ auth/register_process.php
@@ -2,20 +2,20 @@
 require_once("../config.php");
 session_start();
 
-if ($_SERVER["REQUEST_METHOD"] == "post"){
-    $username = $_POST ["username"];
-    $name = $_POST ["name"];
-    $password = $_POST ["password"];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $name = $_POST["name"];
+    $password = $_POST["password"];
     $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-    $Sql = "INSERT INTO users (username, name, password)
-    VALUES ('$username', '$name', '$$hashedPassword')";
-    if ($conn->query($sql)=== TRUE){
-        $_SESSION['notification']=[
+    $sql = "INSERT INTO users (username, name, password)
+    VALUES ('$username', '$name', '$hashedPassword')";
+    if ($conn->query($sql) === TRUE) {
+        $_SESSION['notification'] = [
             'type' => 'primary',
             'message' => 'Registrasi Berhasil!'
         ];
-    } else{
+    } else {
         $_SESSION['notification'] = [
             'type' => 'danger',
             'message' => 'Gagal Regristrasi: '.mysqli_error($conn) 
