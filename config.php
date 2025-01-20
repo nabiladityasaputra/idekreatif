328f66fcc656252ff4db2b97b5644ac0221bfe74
nabiladityasaputra <nabiladityasaputra47@gmail.com>
2025-01-20 15:32:12 +0700

    tugas

--- config.php
+++ config.php
@@ -3,14 +3,14 @@
 //konfigurasi koneksi database
 $host = "localhost";//nama host server database
 $username = "root";//username untuk akses ke database
-$pasword ="";//paswor untuk akses ke databse
+$password ="";//paswor untuk akses ke databse
 $database = "idekreatif";//nama database yg digunakan
 
 // membuat koneksi ke database mengguanakan databse mysqli
 $conn = mysqli_connect($host,$username,$password,$database);
 
 // mengecek apakah koneksi berhasil
-if ($conn->connect_eror){
+if ($conn->connpect_eror){
     //menampilkan pesan eror jika koneksi gagal
     die ("database gagal terkoneksi: " . $conn->connect_eror);
 }
